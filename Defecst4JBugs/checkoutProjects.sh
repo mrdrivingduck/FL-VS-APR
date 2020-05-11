6508e34caa5d2ea76dc14b666667438d58a7a3e4
mrdrivingduck <562655624@qq.com>
2020-05-11 11:14:25 +0800

    Extend d4j2.0 for scripts

--- Defecst4JBugs/checkoutProjects.sh
+++ Defecst4JBugs/checkoutProjects.sh
@@ -1,4 +1,4 @@
-dir=/home/mrdrivingduck/d4jData/ # Store the buggy projects.
+dir=/home/mrdrivingduck/Desktop/d4jData/ # Store the buggy projects.
 
 proj=Chart
 for bug in $(seq 1 26)
@@ -6,32 +6,98 @@ do
 	defects4j checkout -p $proj -v ${bug}b -w ${dir}${proj}_${bug}
 done
 
-# proj=Lang
-# for bug in $(seq 1 65)
-# do
-#         defects4j checkout -p $proj -v ${bug}b -w ${dir}${proj}_${bug}
-# done
+proj=Cli
+for bug in $(seq 1 5; seq 7 40)
+do
+	defects4j checkout -p $proj -v ${bug}b -w ${dir}${proj}_${bug}
+done
 
-# proj=Math
-# for bug in $(seq 1 106)
-# do
-#         defects4j checkout -p $proj -v ${bug}b -w ${dir}${proj}_${bug}
-# done
+proj=Closure
+for bug in $(seq 1 62; seq 64 92; seq 94 176)
+do
+	defects4j checkout -p $proj -v ${bug}b -w ${dir}${proj}_${bug}
+done
 
-# proj=Closure
-# for bug in $(seq 1 133)
-# do
-#         defects4j checkout -p $proj -v ${bug}b -w ${dir}${proj}_${bug}
-# done
+proj=Codec
+for bug in $(seq 1 18)
+do
+	defects4j checkout -p $proj -v ${bug}b -w ${dir}${proj}_${bug}
+done
 
-# proj=Mockito
-# for bug in $(seq 1 38)
-# do
-#         defects4j checkout -p $proj -v ${bug}b -w ${dir}${proj}_${bug}
-# done
+proj=Collections
+for bug in $(seq 25 28)
+do
+	defects4j checkout -p $proj -v ${bug}b -w ${dir}${proj}_${bug}
+done
 
-# proj=Time
-# for bug in $(seq 1 27)
-# do
-# 	defects4j checkout -p $proj -v ${bug}b -w ${dir}${proj}_${bug}
-# done
+proj=Compress
+for bug in $(seq 1 47)
+do
+	defects4j checkout -p $proj -v ${bug}b -w ${dir}${proj}_${bug}
+done
+
+proj=Csv
+for bug in $(seq 1 16)
+do
+	defects4j checkout -p $proj -v ${bug}b -w ${dir}${proj}_${bug}
+done
+
+proj=Gson
+for bug in $(seq 1 18)
+do
+	defects4j checkout -p $proj -v ${bug}b -w ${dir}${proj}_${bug}
+done
+
+proj=JacksonCore
+for bug in $(seq 1 26)
+do
+	defects4j checkout -p $proj -v ${bug}b -w ${dir}${proj}_${bug}
+done
+
+proj=JacksonDatabind
+for bug in $(seq 1 112)
+do
+	defects4j checkout -p $proj -v ${bug}b -w ${dir}${proj}_${bug}
+done
+
+proj=JacksonXml
+for bug in $(seq 1 6)
+do
+	defects4j checkout -p $proj -v ${bug}b -w ${dir}${proj}_${bug}
+done
+
+proj=Jsoup
+for bug in $(seq 1 93)
+do
+	defects4j checkout -p $proj -v ${bug}b -w ${dir}${proj}_${bug}
+done
+
+proj=JxPath
+for bug in $(seq 1 22)
+do
+	defects4j checkout -p $proj -v ${bug}b -w ${dir}${proj}_${bug}
+done
+
+proj=Lang
+for bug in $(seq 1 1; seq 3 65)
+do
+	defects4j checkout -p $proj -v ${bug}b -w ${dir}${proj}_${bug}
+done
+
+proj=Math
+for bug in $(seq 1 106)
+do
+	defects4j checkout -p $proj -v ${bug}b -w ${dir}${proj}_${bug}
+done
+
+proj=Mockito
+for bug in $(seq 1 38)
+do
+	defects4j checkout -p $proj -v ${bug}b -w ${dir}${proj}_${bug}
+done
+
+proj=Time
+for bug in $(seq 1 20; seq 22 27)
+do
+	defects4j checkout -p $proj -v ${bug}b -w ${dir}${proj}_${bug}
+done
--- Defecst4JBugs/compileProjects.sh
+++ Defecst4JBugs/compileProjects.sh
@@ -1,50 +1,154 @@
-dir=/home/mrdrivingduck/d4jData/ # Store the buggy projects.
+dir=/home/mrdrivingduck/Desktop/d4jData/ # Store the buggy projects.
 
 proj=Chart
 for bug in $(seq 1 26)
 do
 	cd ${dir}${proj}_${bug} # ATTENTION!!!!!!
 	defects4j compile
+	echo ${proj}_${bug} done.
 	cd ..
 done
 
-# proj=Lang
-# for bug in $(seq 1 65)
-# do
-# 	cd ${proj}_${bug}
-# 	defects4j compile       
-# 	cd ..
-# done
+proj=Cli
+for bug in $(seq 1 5; seq 7 40)
+do
+	cd ${dir}${proj}_${bug} # ATTENTION!!!!!!
+	defects4j compile
+	echo ${proj}_${bug} done.
+	cd ..
+done
 
-# proj=Math
-# for bug in $(seq 1 106)
-# do
-#         cd ${proj}_${bug}
-#         defects4j compile
-#         cd ..
-# done
+proj=Closure
+for bug in $(seq 1 62; seq 64 92; seq 94 176)
+do
+	cd ${dir}${proj}_${bug} # ATTENTION!!!!!!
+	defects4j compile
+	echo ${proj}_${bug} done.
+	cd ..
+done
 
-# proj=Closure
-# for bug in $(seq 1 133)
-# do
-#         cd ${proj}_${bug}
-#         defects4j compile
-#         cd ..
-# done
+proj=Codec
+for bug in $(seq 1 18)
+do
+	cd ${dir}${proj}_${bug} # ATTENTION!!!!!!
+	defects4j compile
+	echo ${proj}_${bug} done.
+	cd ..
+done
 
-# proj=Mockito
-# for bug in $(seq 1 38)
-# do
-#         cd ${proj}_${bug}
-#         defects4j compile
-#         cd ..
-# done
+proj=Collections
+for bug in $(seq 25 28)
+do
+	cd ${dir}${proj}_${bug} # ATTENTION!!!!!!
+	defects4j compile
+	echo ${proj}_${bug} done.
+	cd ..
+done
 
-# proj=Time
-# for bug in $(seq 1 27)
-# do
-# 	cd ${proj}_${bug}
-# 	defects4j compile
-# 	cd ..
-# done
+proj=Compress
+for bug in $(seq 1 47)
+do
+	cd ${dir}${proj}_${bug} # ATTENTION!!!!!!
+	defects4j compile
+	echo ${proj}_${bug} done.
+	cd ..
+done
 
+proj=Csv
+for bug in $(seq 1 16)
+do
+	cd ${dir}${proj}_${bug} # ATTENTION!!!!!!
+	defects4j compile
+	echo ${proj}_${bug} done.
+	cd ..
+done
+
+proj=Gson
+for bug in $(seq 1 18)
+do
+	cd ${dir}${proj}_${bug} # ATTENTION!!!!!!
+	defects4j compile
+	echo ${proj}_${bug} done.
+	cd ..
+done
+
+proj=JacksonCore
+for bug in $(seq 1 26)
+do
+	cd ${dir}${proj}_${bug} # ATTENTION!!!!!!
+	defects4j compile
+	echo ${proj}_${bug} done.
+	cd ..
+done
+
+proj=JacksonDatabind
+for bug in $(seq 1 112)
+do
+	cd ${dir}${proj}_${bug} # ATTENTION!!!!!!
+	defects4j compile
+	echo ${proj}_${bug} done.
+	cd ..
+done
+
+proj=JacksonXml
+for bug in $(seq 1 6)
+do
+	cd ${dir}${proj}_${bug} # ATTENTION!!!!!!
+	defects4j compile
+	echo ${proj}_${bug} done.
+	cd ..
+done
+
+proj=Jsoup
+for bug in $(seq 1 93)
+do
+	cd ${dir}${proj}_${bug} # ATTENTION!!!!!!
+	defects4j compile
+	echo ${proj}_${bug} done.
+	cd ..
+done
+
+proj=JxPath
+for bug in $(seq 1 22)
+do
+	cd ${dir}${proj}_${bug} # ATTENTION!!!!!!
+	defects4j compile
+	echo ${proj}_${bug} done.
+	cd ..
+done
+
+proj=Lang
+for bug in $(seq 1 1; seq 3 65)
+do
+	cd ${dir}${proj}_${bug} # ATTENTION!!!!!!
+	defects4j compile
+	echo ${proj}_${bug} done.
+	cd ..
+done
+
+proj=Math
+for bug in $(seq 1 106)
+do
+	cd ${dir}${proj}_${bug} # ATTENTION!!!!!!
+	defects4j compile
+	echo ${proj}_${bug} done.
+	cd ..
+done
+
+proj=Mockito
+for bug in $(seq 1 38)
+do
+	cd ${dir}${proj}_${bug} # ATTENTION!!!!!!
+	defects4j compile
+	echo ${proj}_${bug} done.
+	cd ..
+done
+
+proj=Time
+for bug in $(seq 1 20; seq 22 27)
+do
+	cd ${dir}${proj}_${bug} # ATTENTION!!!!!!
+	defects4j compile
+	echo ${proj}_${bug} done.
+	cd ..
+done
